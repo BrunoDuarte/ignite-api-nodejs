4d6a1f1de7b8f0b526a66b0a2c0ec82dc7dc1cff
Bruno Duarte <francisco.duarte@bairesdev.com>
2021-07-18 22:30:50 -0300

    adding avatar

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -21,12 +21,14 @@ class UsersRepository implements IUsersRepository {
     return user
   }
 
-  async create({name, email, driver_licence, password}: ICreateUserDTO): Promise<void> {
+  async create({name, email, driver_licence, password, avatar, id}: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
       email,
       driver_licence,
-      password
+      password, 
+      avatar,
+      id
     })
 
     await this.repository.save(user)
